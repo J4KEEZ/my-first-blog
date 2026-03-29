9517de5b83ee5f6caa3abeaedc5e2847c88c6815
jakekim <jakekim262007@gmail.com>
2026-03-29 15:04:51 +0900

    Add templates and static files

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render 
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
-def home(request):
-    return HttpResponse("Hello, World!")
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
